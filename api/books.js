1c7ce7095f003fc30f15eca3cc995fb439040156
SEAN MCLIMANS <seanmclimans@gmail.com>
2017-07-12 21:01:24 -0600

    add routes for create read update delete

--- api/books.js
+++ api/books.js
@@ -2,12 +2,64 @@ const express = require('express');
 
 const router = express.Router();
 
-const queries = require('../db/queries')
+const queries = require('../db/queries');
+
+function isValidId(req, res, next) {
+  if(!isNaN(req.params.id)) return next();
+  next(new Error('Invalid ID'));
+}
+
+function validBook(book) {
+  const hasTitle = typeof book.title == 'string' && book.title.trim() != ' ';
+  const hasURL = typeof book.cover_url == 'string' && book.cover_url.trim() != ' ';
+  const hasDescription = typeof book.description == 'string' && book.description.trim() != ' ';
+  const hasGenre = typeof book.genre == 'string' && book.genre.trim() != ' ';
+  return hasTitle && hasURL && hasDescription && hasGenre;
+}
 
 router.get('/', (req, res) => {
   queries.getAll().then(books => {
     res.json(books);
-  })
+  });
+});
+
+router.get('/:id', isValidId, (req, res, next) => {
+  queries.getOne(req.params.id).then(book => {
+    if (true) {
+      res.json(books);
+    } else {
+      res.status(404)
+      next(new Error('What the heck is this?'));
+    }
+  });
+});
+
+router.post('/', (req, res, next) => {
+  if(validBook(req.body)) {
+    queries.create(req.body).then(books => {
+      res.json(books[0]);
+    })
+  } else {
+    next(new Error('Invalid book'));
+  }
 })
+
+router.put('/:id', isValidId, (req, res, next) => {
+  if (validBook(req.body)) {
+    queries.update(req.params.id, req.body).then(books => {
+      res.json(books[0])
+    })
+  } else {
+    next(new Error('Invalid book'));
+  }
+});
+
+router.delete('/:id', isValidId, (req, res) => {
+  queries.delete(req.params.id).then(() => {
+    res.json({
+      deleted: true
+    });
+  });
+});
 
 module.exports = router;
--- db/queries.js
+++ db/queries.js
@@ -3,5 +3,17 @@ const knex = require('./knex');
 module.exports = {
   getAll() {
     return knex('books');
+  },
+  getOne(id) {
+    return knex('books').where('id', id).first();
+  },
+  create(book) {
+    return knex('books').insert(book, '*');
+  },
+  update(id, book) {
+    return knex('books').where('id', id).update(book, '*');
+  },
+  delete(id) {
+    return knex('books').where('id', id).del();
   }
 }
